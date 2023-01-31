d0fac05611215749dce4934e0972d725f4b73d19
adam somora <adamsomora33@gmail.com>
2023-01-31 03:01:35 +0100

    add posts model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true, length: { minimum: 5, maximu: 100 }
+  validates :description, presence: true, length: { minimum: 5, maximu: 500 }
+  validates :keywords, presence: true, length: { minimum: 5, maximu: 100 }
 end
