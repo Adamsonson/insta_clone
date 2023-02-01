8adcfa2d59b67154d7569510aefae39249fcf82f
adam somora <adamsomora33@gmail.com>
2023-02-01 01:44:48 +0100

    add upload picture to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :keywords)
+      params.require(:post).permit(:title, :description, :keywords, images: [])
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximu: 100 }
   validates :description, presence: true, length: { minimum: 5, maximu: 500 }
   validates :keywords, presence: true, length: { minimum: 5, maximu: 100 }
+
+  has_many_attached :images
 end
