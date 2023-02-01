04af520dfa3c42664e79d560cadb8802f3fc3fd0
adam somora <adamsomora33@gmail.com>
2023-02-01 02:15:00 +0100

    add devise and sign in up out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+  devise_for :users
+
   get 'home/about'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
