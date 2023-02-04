c80b09886cb9882bda5a25a2e8cc6a5d33821b02
adam somora <adamsomora33@gmail.com>
2023-02-04 01:46:33 +0100

    add randomize id

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,17 @@ class Post < ApplicationRecord
   validates :keywords, presence: true, length: { minimum: 5, maximu: 100 }
 
   has_many_attached :images
+
+  belongs_to :user
+
+  before_create :randomize_id
+
+  private
+
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number
+      (1_000_000_000)
+    end while User.where(id: self.id).exists?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+
+  before_create :randomize_id
+
+  private
+
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number
+      (1_000_000_000)
+    end while User.where(id: self.id).exists?
+  end
 end
