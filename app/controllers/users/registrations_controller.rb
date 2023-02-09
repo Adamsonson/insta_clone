0d43d5ed3065a45dfcdf2071c1ef3e29db262332
adam somora <adamsomora33@gmail.com>
2023-02-09 01:38:20 +0100

    add avatar pic of a user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :bio])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :bio, :avatar])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :bio, :avatar])
   end
 
   protected
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments
+  has_one_attached :avatar
 
   before_create :randomize_id
 
