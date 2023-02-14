84f65f37329d9dfb250bdf49de0af852e36aec31
adam somora <adamsomora33@gmail.com>
2023-02-15 00:32:39 +0100

    refactor profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,7 @@
 class ProfilesController < ApplicationController
   def index
-    @users = users
-  end
-
-  def users
-    query = User.ransack(user_name_cont: search_query)
+    search_query = params[:query]
+    query = User.ransack(username_cont: search_query)
     @users = query.result(distince: true)
-  end
-
-  def search_query
-    params[:query]
   end
 end
